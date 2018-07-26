772f465a26cac3b86efd6e720168fe66b2ac4848
Carlos Sz <me@carlossz.co>
2018-07-26 11:39:18 -0500

    :tada: Dog saga init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { Provider } from 'react-redux'
+import { reducer } from './redux'
+import { watcherSaga } from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+let store = createStore(
+  reducer,
+  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+)
+
+sagaMiddleware.run(watcherSaga)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+  registerServiceWorker();
---
+++ src/redux.js
@@ -0,0 +1,25 @@
+const API_CALL_REQUEST = "API_CALL_REQUEST";
+const API_CALL_SUCCESS = "API_CALL_SUCCESS";
+const API_CALL_ERROR = "API_CALL_ERROR";
+
+const initialState = {
+    fetching: false,
+    dog: null,
+    error: null
+}
+
+export function reducer(state = initialState, action) {
+    switch(action.type) {
+        case API_CALL_REQUEST:
+            return { ...state, fetching: true, error: null };
+        case API_CALL_SUCCESS:
+            return { ...state, fetching: false, dog: action.dog };
+        case API_CALL_ERROR:
+            return { ...state, fetching: false, dog: null, error: action.error };
+        default:
+            return state;
+    }
+}
+
+
+
